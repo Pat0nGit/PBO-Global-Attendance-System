d03d4119b333b4fc3719212a14059f31dd0fe000
Patrix Arman <patrixarman.b@gmail.com>
2025-08-01 00:33:44 +0800

    Final

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5, // Limit to 5 requests per window
+  max: 10, // Limit to 5 requests per window
   message: {
     error: "Too many login attempts. Please try again in 5 minutes.",
   },
